febce021160e58942ad24756e2f2cb272cba70ca
Omar Abbasi <OmarMAbbasi@gmail.com>
2025-06-11 04:19:00 +0000

    uppercase

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/mt2-better-randomizer/"
+  base: "/MT2-better-randomizer/"
 })
