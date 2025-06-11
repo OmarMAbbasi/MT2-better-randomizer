a8db32a92a32e6ff00796fcbd9d273cf1fce6fbc
Omar Abbasi <OmarMAbbasi@gmail.com>
2025-06-11 03:52:26 +0000

    deploying

--- mt2-better-randomizer/vite.config.js
+++ mt2-better-randomizer/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "mt2-better-randomizer"
+  base: "/mt2-better-randomizer/"
 })
