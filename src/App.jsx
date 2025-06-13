a39081f97d7be014236ddcd1fad197e34913269f
Omar Abbasi <OmarMAbbasi@gmail.com>
2025-06-13 17:36:26 +0000

    relative

--- src/App.jsx
+++ src/App.jsx
@@ -141,7 +141,7 @@ function App() {
     <>
       <div>
         <div className="topRow">
-          <img className="mt2Icon" src={`/src/assets/Train.png`} />
+          <img className="mt2Icon" src={`./src/assets/Train.png`} />
            {/* {CLANS.map((clan) => <div className='topItem'><img className="champIcon" src={`/src/assets/${clan}.png`} /></div>)} */}
           {/* <div className='mt2Icon'>MT2 ICON PLACEHOLDER</div> */}
           {CLANS.map((clan) => <div className='topItem'>{clan}</div>)}
